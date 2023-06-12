0834a9ac0ecd2375a18cb572070acd9e870ac488
Anthony Moises Condori Choccata <113246456+Alt-Anthony@users.noreply.github.com>
2023-06-12 10:48:15 -0500

    second use of strip

--- test1.py
+++ test1.py
@@ -1,5 +1,12 @@
 # 1° print
 
-name = input("")
+name = input("").strip()
 
-print("hello, ", name, sep='?',end='!')
+#print("hello, ", name, sep='?',end='!')
+#print(f"this is a {name}, type again to complete the {name}")
+#name = name.title()
+
+#strip() remove whitespaces
+#title() 
+first, last = name.split(" ")
+print(f"this is a {first}, and my name is {last.capitalize()}")
